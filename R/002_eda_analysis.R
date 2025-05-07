82d2c67747cf8feb9aa3c9618f2d2271cb88ad8a
jibarozzo <bolaponte@pm.me>
2025-05-07 11:06:23 -0500

    Minor  change in structure EDA script

--- R/002_eda_analysis.R
+++ R/002_eda_analysis.R
@@ -1,7 +1,9 @@
 #####################################################################
-# NMDS Analysis of Microbial Communities
+# Exploratory Data Analysis of Microbial Communities
 #
-# This script performs Non-Metric Multidimensional Scaling (NMDS)
+# This script performs basi exploratory data analysis for micrbiome data set.
+# Calculates summary statistic, read counts, Good's coverage, prevalence and core
+# taxa analysis (albeit brief) and  Non-Metric Multidimensional Scaling (NMDS)
 # analysis on the phyloseq object to visualize the microbial community
 # structure across different samples.
 #
@@ -14,21 +16,22 @@
 source("R/utils/000_setup.R")
 
 physeq <- readRDS(file = "data/output/processed/sabr_physeq_object.rds")
-# Ensure the phyloseq object (physeq) is loaded before running this script
 
 #--------------------------------------------------------
 # Exploration of data set
 #--------------------------------------------------------
-## Checking sampling effort, read counts, etc.
-colnames(tax_table(physeq)) # Display column names for reference
+## Basic metadata exploration
+colnames(tax_table(physeq))
 colnames(sample_data(physeq))
 
-## Summaries
 #--------------------------------------------------------
-metagMisc::phyloseq_summary(physeq, more_stats = F, long = F) # metagMisc overall summary
-microbiome::summarize_phyloseq(physeq) #Microbiome package summary
+# Summaries and Read Counts
+#--------------------------------------------------------
+metagMisc::phyloseq_summary(physeq, more_stats = F, long = F)
+microbiome::summarize_phyloseq(physeq)
+# All samples have at least 5000 reads
 
-# How many samples with ASVs per phyla?
+# Taxonomic distribution
 percent_phyla_clean <- phyloseq_ntaxa_by_tax(
   physeq,
   TaxRank = "phylum",
@@ -38,14 +41,90 @@ percent_phyla_clean <- phyloseq_ntaxa_by_tax(
   as.data.frame() |>
   mutate(sum = sum(N.OTU)) |>
   group_by(phylum) |>
-  summarise(occurance_in_samples = n()) # Count the number of ASV per phylum
+  summarise(occurance_in_samples = n())
 
-
-## Coverage
 #--------------------------------------------------------
-cover_chao <- phyloseq_coverage(physeq, correct_singletons = T, add_attr = T) #Coverage: Good-Turing frequency estimation (Chiu, Chao 2016)
+# Read Count Analysis
+#--------------------------------------------------------
+names_list <- colnames(physeq@otu_table)
 
-## Prevalence plot
+reads <- readcount(physeq) |>
+  as.data.frame() |>
+  rownames_to_column(var = "sample_id") |>
+  rename(n_seqs = "readcount(physeq)") |>
+  dplyr::right_join(
+    rownames_to_column(metadata, var = "sample_id"),
+    by = "sample_id"
+  ) |>
+  filter(sample_id %in% names_list)
+
+reads_sum <- reads |>
+  group_by(sample_id) |>
+  filter(sample_id %in% names_list) |>
+  summarise(n_seqs = sum(n_seqs)) |>
+  arrange(n_seqs)
+
+#--------------------------------------------------------
+# Read Count Visualizations
+#--------------------------------------------------------
+ggplot(reads, aes(x = n_seqs)) +
+  geom_density(position = "identity", stat = "density", fill = "red") +
+  #geom_histogram(binwidth = 250, fill = "grey", color = "black") +
+  coord_cartesian(xlim = c(0, 40000))
+
+ggplot(reads, aes(x = 1, y = n_seqs)) +
+  geom_jitter() +
+  scale_y_log10()
+
+ggplot(
+  reads |>
+    arrange(n_seqs) |>
+    filter(sample_id %in% names_list),
+  aes(x = 1:nrow(test), y = n_seqs)
+) +
+  geom_line() +
+  coord_cartesian(ylim = c(0, 100000))
+
+reads |>
+  arrange(n_seqs) |>
+  filter(sample_id %in% names_list)
+
+#--------------------------------------------------------
+# Good's Coverage Analysis
+#--------------------------------------------------------
+## Coverage estimates
+cover_chao <- phyloseq_coverage(physeq, correct_singletons = T, add_attr = T)
+
+ps_melt <- physeq |>
+  psmelt() |>
+  janitor::clean_names() |>
+  rename(sample_id = sample, asv = otu) |>
+  select(!nitrogen_conc)
+
+cover_goods <- ps_melt |>
+  select(sample_id, plant, asv, abundance) |>
+  group_by(sample_id, plant) |>
+  summarise(
+    n_seqs = sum(abundance),
+    n_singletons = sum(abundance == 1),
+    goods = 1 - (n_singletons / n_seqs)
+  )
+
+cover_goods |>
+  #filter(n_seqs > 750) |>
+  ggplot(aes(x = n_seqs, y = goods, color = plant)) +
+  geom_point()
+
+cover_goods |>
+  filter(n_seqs > 750) |>
+  arrange(goods)
+
+
+#--------------------------------------------------------
+# Prevalence & Core Microbiome Analysis
+#--------------------------------------------------------
+
+## Prevalence visualization
 phyloseq_prevalence_plot(
   physeq,
   prev.trh = 0.5,
@@ -55,7 +134,7 @@ phyloseq_prevalence_plot(
   showplot = T
 )
 
-## Average relative ASVs per host plant
+## Host plant-specific averages
 ps_average <- phyloseq_average(
   physeq,
   avg_type = "arithmetic",
@@ -68,121 +147,51 @@ ps_average <- phyloseq_average(
   progress = "text",
 )
 
-## Core ASV abundance
+## Core microbiome analysis
 core_abundance(
   physeq@otu_table,
-  detection = 0.1 / 100,
+  detection = 0.1,
   prevalence = 50 / 100,
   include.lowest = FALSE
 )
 
-## Rare ASVs
 rare_abundance(
   physeq@otu_table,
-  detection = 0.1 / 100,
+  detection = 0.1,
   prevalence = 50 / 100,
   include.lowest = FALSE
 )
 
-## Core taxa
 core_tax <- core_members(
   physeq,
-  detection = 0.1 / 100,
+  detection = 0.1,
   prevalence = 50 / 100,
   include.lowest = FALSE
 )
 
-## Rare taxa
 rare_tax <- rare_members(
   physeq,
-  detection = 0.1 / 100,
+  detection = 0.1,
   prevalence = 50 / 100,
   include.lowest = FALSE
 )
 
-# Reads per sample
-names_list <- colnames(physeq@otu_table) #List of samples
-
-reads <- readcount(physeq) |>
-  as.data.frame() |>
-  rownames_to_column(var = "sample_id") |>
-  rename(n_seqs = "readcount(physeq)") |>
-  dplyr::right_join(
-    rownames_to_column(metadata, var = "sample_id"),
-    by = "sample_id"
-  ) |>
-  filter(sample_id %in% names_list)
-
-# Sum of reads per site per species
-reads_sum <- reads |>
-  group_by(plot, plant) |>
-  filter(sample_id %in% names_list) |>
-  summarise(n_seqs = sum(n_seqs))
-
-# Visualizing the distribution of reads
-ggplot(reads, aes(x = n_seqs)) +
-  geom_histogram(binwidth = 10000, fill = "grey", color = "black") +
-  coord_cartesian(xlim = c(0, 100000)) # Cut-off
-
-ggplot(reads, aes(x = 1, y = n_seqs)) +
-  geom_jitter() +
-  scale_y_log10()
-
-test <- reads |>
-  arrange(n_seqs) |>
-  filter(sample_id %in% names_list)
-
-ggplot(test, aes(x = 1:nrow(test), y = n_seqs)) +
-  geom_line() +
-  coord_cartesian(ylim = c(0, 100000)) # Cut-off
-
-
-# Arrange the samples to see where the big breaks in counts are.
-reads |>
-  arrange(n_seqs) |>
-  filter(sample_id %in% names_list)
-
-#Good's coverage
-ps_melt <- physeq |>
-  psmelt() |>
-  janitor::clean_names() |>
-  rename(sample_id = sample, asv = otu) |>
-  select(!nitrogen_conc)
-
-
-cover_goods <- ps_melt |>
-  select(sample_id, plant, ASV, Abundance) |>
-  group_by(sample_id) |>
-  summarise(
-    n_seqs = sum(Abundance),
-    n_sing = sum(Abundance == 1),
-    goods = 1 - (n_sing / n_seqs)
-  )
-cover_goods |>
-  filter(n_seqs > 750) |> #Filtering samples with more than f000 reads
-  ggplot(aes(x = n_seqs, y = goods)) +
-  geom_point() # Similar to `phyloseq_coverage` results above but here we know how it is calculated.
-
-cover_goods |>
-  filter(n_seqs > 750) |>
-  arrange(goods)
-
-
 #------------------------------------------------------
+# Community Diversity
+#--------------------------------------------------------
 
-plot_bar(physeq, x = "samples", fill = "phylum")
-
+#plot_bar(physeq, x = "samples", fill = "phylum")
 
 ## Richness
 
 plot_richness(
   physeq,
-  x = "samples",
-  measures = c("Shannon", "Simpson"),
-  color = "sampling_location",
+  x = "plot",
+  measures = c("Observed", "Shannon", "Simpson"),
+  color = "plant",
+  shape = "sampling_location",
   scales = "free"
 )
-
 
 #--------------------------------------------------------
 # Calculate Bray-Curtis distance and perform NMDS analysis
@@ -195,12 +204,6 @@ bc_dist <- phyloseq::distance(physeq, method = "bray")
 # trymax=100 ensures convergence by trying up to 100 different random starts
 nmds <- ordinate(physeq, method = "NMDS", distance = "bray", trymax = 100)
 
-#--------------------------------------------------------
-# Prepare data for visualization
-#--------------------------------------------------------
-
-# Convert "Date" to a factor for proper panel separation in visualization
-#sample_data(physeq)$Date <- as.factor(sample_data(physeq)$Date)
 
 #--------------------------------------------------------
 # Generate and display NMDS plot
