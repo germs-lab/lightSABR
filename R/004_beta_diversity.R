86a9741045287633cae1c58cf621c780bdd5255e
jibarozzo <bolaponte@pm.me>
2025-09-22 14:16:27 -0500

    Starting dbRDA analyses

--- R/004_beta_diversity.R
+++ R/004_beta_diversity.R
@@ -13,7 +13,7 @@
   source("R/utils/000_setup.R")
 
 #--------------------------------------------------------
-# Calculate Bray-Curtis distance and perform NMDS analysis
+# Section 1: Calculate Bray-Curtis distance and perform NMDS analysis
 #--------------------------------------------------------
 
 # Calculate Bray-Curtis distance matrix
@@ -43,55 +43,9 @@ BRCore::brc_gg_ordi(.data = main_raw_nmds$nmds_df, .color = plant, "NMDS") +
   theme_minimal()
 
 
-# Needs fine tuning
-
-# Perform NMDS analysis (k=2 reduces to 2 dimensions)
-# trymax=100 ensures convergence by trying up to 100 different random starts
-nmds <- ordinate(
-  sabr_2023_physeq,
-  method = "NMDS",
-  distance = "bray",
-  trymax = 500
-)
-
 #--------------------------------------------------------
-# Generate and display NMDS plot
-#--------------------------------------------------------
-
-# # Create NMDS plot with samples colored by Plant, shaped by Location, and faceted by Date
-# nmds_plot <- plot_ordination(
-#   sabr_2023_physeq,
-#   nmds,
-#   color = "plant",
-#   shape = "sampling_location"
-# ) +
-#   geom_point(size = 3, alpha = 0.8) +
-#   facet_wrap(~sampling_date, ncol = 2) + # Separate panels by Date
-#   labs(
-#     title = "NMDS of Microbial Communities by Date",
-#     x = "NMDS1",
-#     y = "NMDS2"
-#   ) +
-#   theme_minimal() +
-#   theme(
-#     legend.position = "right",
-#     plot.title = element_text(hjust = 0.5, face = "bold")
-#   )
-
-# # Display the plot
-# nmds_plot
-
-# # Optionally, save the plot
-# ggsave(
-#   "data/output/plots/nmds_plot.png",
-#   nmds_plot,
-#   width = 10,
-#   height = 8,
-#   dpi = 300
-# )
-
-#--------------------------------------------------------
-# Gnerate NMDS from phyloseq object at different thresholds
+# Generate NMDS from phyloseq
+# object at different thresholds
 #--------------------------------------------------------
 
 prevalence_list <- list(
@@ -138,30 +92,32 @@ prevalence_nmds_map <- purrr::imap(
   }
 )
 
-# Verifying that all calculations are based on their corresponding matrices.
-purrr::imap(prevalence_nmds_map, function(res, nm) {
-  cat("\n---", nm, "---\n")
-  if (is.null(res)) {
-    cat("Result is NULL\n")
-  } else {
-    print(head(res$nmds_scores))
-    cat("Stress:", res$ordi$stress, "\n")
-  }
-  invisible(NULL)
-})
+# Verifying that all calculations are based on
+# their corresponding matrices.
+# Use to double check things. If all is good, skip.
+# purrr::imap(prevalence_nmds_map, function(res, nm) {
+#   cat("\n---", nm, "---\n")
+#   if (is.null(res)) {
+#     cat("Result is NULL\n")
+#   } else {
+#     print(head(res$nmds_scores))
+#     cat("Stress:", res$ordi$stress, "\n")
+#   }
+#   invisible(NULL)
+# })
 
-sapply(prevalence_list, function(ps) {
-  m <- as(phyloseq::otu_table(ps), "matrix")
-  if (phyloseq::taxa_are_rows(ps)) {
-    m <- t(m)
-  }
-  message(
-    ": dim=",
-    paste(dim(m), collapse = "x"),
-    " hash=",
-    digest::digest(m)
-  )
-})
+# sapply(prevalence_list, function(ps) {
+#   m <- as(phyloseq::otu_table(ps), "matrix")
+#   if (phyloseq::taxa_are_rows(ps)) {
+#     m <- t(m)
+#   }
+#   message(
+#     ": dim=",
+#     paste(dim(m), collapse = "x"),
+#     " hash=",
+#     digest::digest(m)
+#   )
+# })
 
 # Plots should all be different
 
@@ -183,12 +139,37 @@ nmds_df_list <- list(
   prevalent_30 = prevalence_nmds_map$prevalent_30$nmds_df
 )
 
+#--------------------------------------------------------
+# Visualize NMDS plots
+#--------------------------------------------------------
+
 beta_nmds_plots <-
   imap(nmds_df_list, function(nmds_df, prev_label) {
     nmds_df <- nmds_df %>% mutate(prevalence_level = prev_label)
-    # Make a nice title from the name "prevalent30" -> "Prevalent 30%"
-    title_label <- prev_label %>%
-      str_replace("^prevalent_?(\\d+).*", "ASVs Prevalent at \\1% occupancy")
+    # Nice title
+    # Map "prevalent30" or "prevalent_30" -> "prevalent_30" to access the full result
+    prev_key <- stringr::str_replace(
+      prev_label,
+      "^prevalent_?(\\d+).*",
+      "prevalent_\\1"
+    )
+
+    # Percentages
+    pct <- stringr::str_extract(prev_label, "\\d+")
+
+    # ASVs used in the NMDS = number of species scores rows
+    asv_count <- nrow(vegan::scores(
+      prevalence_nmds_map[[prev_key]]$ordi,
+      display = "species"
+    ))
+
+    title_label <- bquote(
+      "ASVs Prevalent at " *
+        .(pct) *
+        "% occupancy (" *
+        italic(n) ==
+        .(asv_count) * ")"
+    )
 
     p <- BRCore::brc_gg_ordi(
       .data = nmds_df,
@@ -204,152 +185,100 @@ beta_nmds_plots <-
   })
 
 beta_nmds_plots
-# # Extract OTU table for high-prevalence ASVs
-# asv_high_prev <- asv_table_data[high_prevalence_asvs, ]
-# asv_high_prev <- otu_table(asv_high_prev, taxa_are_rows = TRUE)
 
-# # Create a new phyloseq object with only high-prevalence ASVs
-# ps_high_prev <- merge_phyloseq(ps_rel, asv_high_prev)
+#--------------------------------------------------------
+# Section 2: dbRDAs and PERMANOVAs
+#--------------------------------------------------------
+#--------------------------------------------------------
+# dbRDA BC_CORE COMMUNITY ANALYSIS
+#--------------------------------------------------------
+# Transform data using Hellinger transformation
+# Hellinger transformation
 
-# #--------------------------------------------------------
-# # NMDS analysis for high-prevalence ASVs
-# #--------------------------------------------------------
+asv_matrices_list <- list(
+  asvs_100pct = as(phyloseq::otu_table(sabr_2023_physeq), "matrix"),
+  asvs_90pct = as(
+    phyloseq::otu_table(main_physeq_list$original_phyloseq_lists$prevalent_90),
+    "matrix"
+  ),
+  asvs_80pct = as(
+    phyloseq::otu_table(main_physeq_list$original_phyloseq_lists$prevalent_80),
+    "matrix"
+  ),
+  asvs_70pct = as(
+    phyloseq::otu_table(main_physeq_list$original_phyloseq_lists$prevalent_70),
+    "matrix"
+  ),
+  asvs_60pct = as(
+    phyloseq::otu_table(main_physeq_list$original_phyloseq_lists$prevalent_60),
+    "matrix"
+  ),
+  asvs_30pct = as(
+    phyloseq::otu_table(main_physeq_list$original_phyloseq_lists$prevalent_30),
+    "matrix"
+  )
+)
 
-# # Calculate Bray-Curtis distance for the high-prevalence dataset
-# bc_dist_high_prev <- phyloseq::distance(ps_high_prev, method = "bray")
+hellinger_matrices_list <- purrr::map(asv_matrices_list, function(asv_matrix) {
+  hell_matrix <- decostand(asv_matrix, MARGIN = 1, method = "hellinger") #Rows are taxa
 
-# # Perform NMDS for high-prevalence ASVs
-# nmds_high_prev <- ordinate(
-#   ps_high_prev,
-#   method = "NMDS",
-#   distance = "bray",
-#   trymax = 100
-# )
+  hell_matrix_clean <- hell_matrix %>%
+    as.data.frame() %>%
+    select(where(~ is.numeric(.) && sum(.) > 0)) %>%
+    # rownames_to_column(., var = "id") %>%
+    # mutate(sample_id = id) %>%
+    # column_to_rownames(., var = "id") %>%
+    # relocate(., sample_id) %>%
+    as.matrix()
 
-# # Create and display NMDS plot for highly prevalent ASVs
-# nmds_plot_high_prev <- plot_ordination(
-#   ps_high_prev,
-#   nmds_high_prev,
-#   color = "Plant",
-#   shape = "Location"
-# ) +
-#   geom_point(size = 3, alpha = 0.8) +
-#   facet_wrap(~Date, ncol = 2) +
-#   labs(
-#     title = "NMDS of Highly Prevalent OTUs",
-#     x = "NMDS1",
-#     y = "NMDS2"
-#   ) +
-#   theme_minimal() +
-#   theme(
-#     legend.position = "right",
-#     plot.title = element_text(hjust = 0.5, face = "bold")
-#   )
+  return(hell_matrix_clean)
+})
 
-# print(nmds_plot_high_prev)
 
-# #--------------------------------------------------------
-# # Identify and analyze low-prevalence ASVs (<70% samples)
-# #--------------------------------------------------------
+# Prepare environmental data
+dbrda_traits <- sabr_2023_metadata_clean %>%
+  select(., c(plot:sampling_location, sampling_date, gnha:gwc_g_g)) %>%
+  dplyr::filter(
+    rownames(.) %in% colnames(hellinger_matrices_list$asvs_100pct)
+  ) %>%
+  rownames_to_column(., var = "id") %>%
+  mutate(sample_id = id) %>%
+  column_to_rownames(., var = "id") %>%
+  relocate(., sample_id)
 
-# # Define threshold for low prevalence (70% of samples)
-# threshold_u70 <- total_samples * 0.7
-# low_prevalence_asvs_u70 <- names(asv_sample_counts[
-#   asv_sample_counts < threshold_u70
-# ])
+# Build dbRDA models with different constraints
+dbrda_00 <- capscale(
+  hellinger_matrices_list$asvs_90pct ~ 1,
+  distance = "bray",
+  dfun = vegdist,
+  data = dbrda_traits,
+  parallel = 2,
+  na.action = na.omit
+)
+str(dbrda_00)
 
-# # Extract taxonomy for low-prevalence ASVs
-# low_prevalence_taxa_u70 <- tax_table_data[low_prevalence_asvs_u70, ]
+dbrda_01_bc_core <- dbrda(
+  t(hell_matrix) ~ .,
+  distance = "bray",
+  dfun = vegdist,
+  data = dbrda_traits,
+  parallel = 8,
+  na.action = na.omit
+)
 
-# # Display information about low-prevalence ASVs
-# cat("ASVs found in <70% samples:", length(low_prevalence_asvs_u70), "\n")
-# print(low_prevalence_taxa_u70)
-
-# # Create a new phyloseq object with only low-prevalence ASVs
-# asv_low_prev_u70 <- otu_table_data[, low_prevalence_asvs_u70]
-# asv_low_prev_u70 <- otu_table(asv_low_prev_u70, taxa_are_rows = FALSE)
-# ps_low_prev_u70 <- merge_phyloseq(ps_rel, asv_low_prev_u70)
-
-# # NMDS for low-prevalence ASVs (<70%)
-# bc_dist_low_prev_u70 <- phyloseq::distance(ps_low_prev_u70, method = "bray")
-# nmds_low_prev_u70 <- ordinate(
-#   ps_low_prev_u70,
-#   method = "NMDS",
-#   distance = "bray",
-#   trymax = 100
-# )
-
-# # Create and display NMDS plot for low-prevalence ASVs (<70%)
-# nmds_plot_low_prev_u70 <- plot_ordination(
-#   ps_low_prev_u70,
-#   nmds_low_prev_u70,
-#   color = "Plant",
-#   shape = "Location"
-# ) +
-#   geom_point(size = 3, alpha = 0.8) +
-#   facet_wrap(~Date, ncol = 2) +
-#   labs(
-#     title = "NMDS of Low Prevalence OTUs (Present in <70% of Samples)",
-#     x = "NMDS1",
-#     y = "NMDS2"
-#   ) +
-#   theme_minimal() +
-#   theme(
-#     legend.position = "right",
-#     plot.title = element_text(hjust = 0.5, face = "bold")
-#   )
-
-# print(nmds_plot_low_prev_u70)
-
-# #--------------------------------------------------------
-# # Identify and analyze very low-prevalence ASVs (<30% samples)
-# #--------------------------------------------------------
-
-# # Define threshold for very low prevalence (30% of samples)
-# threshold_u30 <- total_samples * 0.3
-# low_prevalence_asvs_u30 <- names(asv_sample_counts[
-#   asv_sample_counts < threshold_u30
-# ])
-
-# # Extract taxonomy for very low-prevalence ASVs
-# low_prevalence_taxa_u30 <- tax_table_data[low_prevalence_asvs_u30, ]
-
-# # Display information about very low-prevalence ASVs
-# cat("ASVs found in <30% samples:", length(low_prevalence_asvs_u30), "\n")
-# print(low_prevalence_taxa_u30)
-
-# # Create a new phyloseq object with only very low-prevalence ASVs
-# asv_low_prev_u30 <- otu_table_data[, low_prevalence_asvs_u30]
-# asv_low_prev_u30 <- otu_table(asv_low_prev_u30, taxa_are_rows = FALSE)
-# ps_low_prev_u30 <- merge_phyloseq(ps_rel, asv_low_prev_u30)
-
-# # NMDS for very low-prevalence ASVs (<30%)
-# bc_dist_low_prev_u30 <- phyloseq::distance(ps_low_prev_u30, method = "bray")
-# nmds_low_prev_u30 <- ordinate(
-#   ps_low_prev_u30,
-#   method = "NMDS",
-#   distance = "bray",
-#   trymax = 100
-# )
-
-# # Create and display NMDS plot for very low-prevalence ASVs (<30%)
-# nmds_plot_low_prev_u30 <- plot_ordination(
-#   ps_low_prev_u30,
-#   nmds_low_prev_u30,
-#   color = "Plant",
-#   shape = "Location"
-# ) +
-#   geom_point(size = 3, alpha = 0.8) +
-#   facet_wrap(~Date, ncol = 2) +
-#   labs(
-#     title = "NMDS of Low Prevalence OTUs (Present in <30% of Samples)",
-#     x = "NMDS1",
-#     y = "NMDS2"
-#   ) +
-#   theme_minimal() +
-#   theme(
-#     legend.position = "right",
-#     plot.title = element_text(hjust = 0.5, face = "bold")
-#   )
-
-# print(nmds_plot_low_prev_u30)
+dbrda_02 <- dbrda(
+  t(hellinger_matrices_list$asvs_90pct) ~ plot + plant,
+  distance = "bray",
+  dfun = vegdist,
+  data = dbrda_traits,
+  parallel = 2,
+  na.action = na.omit
+)
+BRCore::brc_flex_ordi(
+  dbrda_02,
+  dbrda_traits,
+  color_var = "plant",
+  sample_id_col = "sample_id",
+  biplot = TRUE,
+  biplot_n = 18
+)
